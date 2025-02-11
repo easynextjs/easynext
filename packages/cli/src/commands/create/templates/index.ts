202998f6d46dd171e6f1b8df879c841c3aefbd5b
greatSumini <greatSumini@gmail.com>
2025-02-11 16:21:02 +0900

    feat(cli/template): update template

--- packages/cli/src/commands/create/templates/index.ts
+++ packages/cli/src/commands/create/templates/index.ts
@@ -200,6 +200,7 @@ function addDependencies(packageJson: any) {
   // useful dependencies
   packageJson.dependencies = {
     ...packageJson.dependencies,
+    '@hookform/resolvers': '^4',
     'react-use': '^17',
     'ts-pattern': '^5',
     'es-toolkit': '^1',

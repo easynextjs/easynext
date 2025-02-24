a1468ad7cf8139d2ddf3fa44df49033a6a556653
greatSumini <greatSumini@gmail.com>
2025-02-24 17:31:25 +0900

    fix

--- webpage/src/app/api/premium/template-url/route.ts
+++ webpage/src/app/api/premium/template-url/route.ts
@@ -3,21 +3,21 @@ import { NextResponse } from "next/server";
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-const s3Client = new S3Client({
-  region: "auto",
-  endpoint: process.env.CLOUDFLARE_R2_ENDPOINT,
-  credentials: {
-    accessKeyId: process.env.CLOUDFLARE_R2_ACCESS_KEY,
-    secretAccessKey: process.env.CLOUDFLARE_R2_SECRET_KEY,
-  },
-  forcePathStyle: true,
-});
-
 async function generatePresignedUrl(
   bucketName: string,
   objectKey: string,
   expiresInSec = 60
 ) {
+  const s3Client = new S3Client({
+    region: "auto",
+    endpoint: process.env.CLOUDFLARE_R2_ENDPOINT,
+    credentials: {
+      accessKeyId: process.env.CLOUDFLARE_R2_ACCESS_KEY,
+      secretAccessKey: process.env.CLOUDFLARE_R2_SECRET_KEY,
+    },
+    forcePathStyle: true,
+  });
+
   const command = new GetObjectCommand({
     Bucket: bucketName,
     Key: objectKey,

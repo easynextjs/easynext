d644190c2ec465ea5521bd3426dbd219e6e8115d
greatSumini <greatSumini@gmail.com>
2025-03-06 15:31:49 +0900

    build(cli): :rocket: release v0.1.32

--- packages/cli/src/commands/create/templates/default/src/app/page.tsx
+++ packages/cli/src/commands/create/templates/default/src/app/page.tsx
@@ -7,7 +7,7 @@ import { useToast } from '@/hooks/use-toast';
 import axios from 'axios';
 
 const PACKAGE_NAME = '@easynext/cli';
-const CURRENT_VERSION = 'v0.1.24';
+const CURRENT_VERSION = 'v0.1.32';
 
 function latestVersion(packageName: string) {
   return axios
